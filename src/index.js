669dea9b13e6d02938be1309afe11f2bc43af9ff
jackSparrow381 <developerasad381@gmail.com>
2022-09-08 10:24:18 +0500

    Set Theming

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
+import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
 
